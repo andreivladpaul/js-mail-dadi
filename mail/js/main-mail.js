9a04598b815e84f579353c36c4aab551d1c71e42
Vlad <andreivladpaul@gmail.com>
2021-07-16 14:34:15 +0200

    Esercizio mail completo

---
+++ mail/js/main-mail.js
@@ -0,0 +1,17 @@
+/* 
+Chiedi all’utente la sua email,
+
+controlla che sia nella lista di chi può accedere,
+
+stampa un messaggio appropriato sull’esito del controllo. */
+
+
+var mailUtente = prompt("Qual è la tua mail?");
+
+var mailList = ["paolodossetto@gmail.com", "dariobosco@gmail.com", "vladandrei@gmail.com", "hamzajamil@gmailcom", "vitomarseglia@gmail.com"];
+
+if (mailList.includes(mailUtente)) {
+    alert("Bentornato nel tuo spazio riservato!");
+} else {
+    alert("Accesso negato. Riprova.");
+}
